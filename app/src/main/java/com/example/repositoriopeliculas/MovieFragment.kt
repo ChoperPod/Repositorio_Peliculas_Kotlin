714f5308de672b69906007df2eea902d0156144c
Jose Alfredo Garcia Villanueva <j.alfredo.garcia.villanueva@gmail.com>
2021-03-26 17:20:39 -0600

    Commit Fin de seccion 4 del curso de desarrollo android en kotlin

--- app/src/main/java/com/example/repositoriopeliculas/MovieFragment.kt
+++ app/src/main/java/com/example/repositoriopeliculas/MovieFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.repositoriopeliculas.databinding.ActivityMainBinding
+import com.example.repositoriopeliculas.databinding.FragmentMovieBinding
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    private lateinit var binding: FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
     }
 }
